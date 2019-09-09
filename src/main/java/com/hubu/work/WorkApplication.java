c061e889d3264635cb97363008a513568ea349cf
2240428093 <2240428093@qq.com>
2019-09-09 11:01:39 +0800

    第一次修改

--- src/main/java/com/hubu/work/WorkApplication.java
+++ src/main/java/com/hubu/work/WorkApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WorkApplication {
 
   public static void main(String[] args) {
+
     SpringApplication.run(WorkApplication.class, args);
+
+    System.out.println("尝试一下提交");
   }
 
 }

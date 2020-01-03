069c9b3fab385313a3e294fec4ed6dbbff7b6bab
Your Name <you@example.com>
2020-01-03 21:32:55 +0800

    歌曲查询

---
+++ src/main/java/com/hubu/work/H2_MyBatis/mapper/SongSheetMapper.java
@@ -0,0 +1,12 @@
+package com.hubu.work.H2_MyBatis.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface SongSheetMapper {
+    List<SongSheet> selectAll();//查询歌单名
+}
